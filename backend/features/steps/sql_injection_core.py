309c15d54e7f1a7a82428834e34cdf2de9878434
cR4zYD0G <frlc_31@hotmail.com>
2019-10-17 23:38:51 -0300

    commit partial implement sqli

--- backend/features/steps/sql_injection_core.py
+++ backend/features/steps/sql_injection_core.py
@@ -2,11 +2,38 @@ import common.requests_generic as rg
 import re
 import subprocess
 import time
+import json
 
 result = ''
+api_header = {'Content-Type' : 'application/json'}
 
 def sql_injection_initial(url, method, header_value, body_value, sqlmap_url, id_scan=None):
     result = sqlmap_get_status(sqlmap_url)
+    if result is True:
+        taskid = get_new_task_id(sqlmap_url)
+    if taskid:
+        # Taskid is created.
+        set_option_status = set_options_list(url,method,header_value,body_value,taskid, sqlmap_url)
+        if set_option_status is True:
+            # Everything is set to start the scan
+            start_scan_result = start_scan(taskid)
+            if start_scan_result is True:
+                print("SQLi - Scan started.")
+                result = scan_status(taskid)
+                if result is True:
+                    # API is vulnerable
+                    print ("[+] is vulnerable to SQL injection")
+                    attack_result = { "id" : 10, "scanid" : scanid, "url" : url, "alert": "SQL injection", "impact": "High", "req_headers": headers, "req_body":body, "res_headers": "NA" ,"res_body": "NA", "log" : scan_data}
+                    dbupdate.insert_record(attack_result)
+
+        else:
+            logs.logging.info("Sqli - Failed to create a task.") 
+
+        task_result = delete_task(taskid)
+        if task_result is True:
+            # Task deleted successfully
+            logs.logging.info("SQLi - Task deleted: %s",taskid)
+
 
 def sqlmap_get_status(sqlmap_url):
     try:
@@ -16,7 +43,7 @@ def sqlmap_get_status(sqlmap_url):
             return True
     except:
         result = sqlmap_start_process()
-        # return result
+        return result
 
 def sqlmap_start_process():
     try:
@@ -25,7 +52,6 @@ def sqlmap_start_process():
         if out:
             out_temp = str(out)
             location_path = out_temp[out_temp.find('Location:')+10:]
-            # location_path = str(location_path)
             location_path_temp = location_path.split('\\n')
             sqlmap_path = location_path_temp[0] + '/sqlmap/sqlmapapi.py'
             if sqlmap_path:
@@ -39,4 +65,24 @@ def sqlmap_start_process():
                         return True
     except:
         print('Failed to start sqlmap')
-        pass      
+        return      
+
+def get_new_task_id(sqlmap_url):
+    # Create a new task for scan
+    new_task_url = sqlmap_url + "/task/new"
+    new_task = rg.send_request_generic(new_task_url,"GET", api_header)
+    if new_task.status_code == 200:
+        return json.loads(new_task.text)['taskid']
+
+def set_options_list(url, method, headers, body, task_id, sqlmap_url):
+    # Setting up url,headers, body for scan
+    options_set_url = sqlmap_url + "/option/" + task_id + "/set"
+    data = {}
+    data['url'], data['method'], data['headers'] = url, method, headers
+    if method.upper() == 'POST' or method.upper() == 'PUT':
+        if headers['Content-type'] == 'application/json':
+            body=json.dumps(body)
+        data['data'] = body
+    options_list = rg.send_request_generic(options_set_url, "POST", api_header, data)
+    if options_list.status_code == 200:
+        return json.loads(options_list.text)['success']        
